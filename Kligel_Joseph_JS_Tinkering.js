e46906a0f05618d16e88e7d5ac6c51ecb8498806
Joseph Kligel <jkligel@gmail.com>
2022-10-24 20:25:30 -0400

    Changing directories

---
+++ Kligel_Joseph_JS_Tinkering.js
@@ -0,0 +1,10 @@
+let childHeight = 24
+
+function displayIfChildIsAbleToRideTheRollerCoaster(height){
+    if(height > 52) console.log("Get on that ride, kiddo!")
+
+    else console.log("Sorry kiddo. Maybe next year.")
+
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster(53);
